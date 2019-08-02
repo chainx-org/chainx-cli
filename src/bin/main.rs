b44d974bc8f150d574d54976b4300dbf792b3fb0
koushiro <koushiro.cqx@gmail.com>
2019-08-02 18:03:29 +0800

    Finish demo
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,6 +2,7 @@ use chainx_cli::*;
 
 fn main() -> Result<()> {
     let cmd = cli::init();
-    println!("{:?}", cmd);
+    let (_handle, chainx) = http_connect("http://47.99.192.159:8086")?;
+    cmd.dispatch(chainx)?;
     Ok(())
 }
