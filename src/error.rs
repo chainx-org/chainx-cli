8e967ac4a8c2d60b30ecedee7de8a349f8549918
Qinxuan Chen <koushiro.cqx@gmail.com>
2020-05-30 22:53:23 +0800

    Follow ChainX master (#16)
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- src/error.rs
+++ src/error.rs
@@ -1,39 +1,15 @@
-use failure::Fail;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("Format error: {0}")]
+    Fmt(#[from] std::fmt::Error),
+    #[error("Io error: {0}")]
+    Io(#[from] std::io::Error),
+    #[error("Json error: {0}")]
+    Json(#[from] serde_json::Error),
+    #[error("Rpc error: {0}")]
+    Rpc(#[from] web3::Error),
+}
 
 pub type Result<T> = std::result::Result<T, Error>;
-
-#[derive(Debug, Fail)]
-pub enum Error {
-    #[fail(display = "Format error: {}", _0)]
-    Fmt(#[cause] std::fmt::Error),
-    #[fail(display = "Io error: {}", _0)]
-    Io(#[cause] std::io::Error),
-    #[fail(display = "Json error: {}", _0)]
-    Json(#[cause] serde_json::Error),
-    #[fail(display = "{}", _0)]
-    Rpc(#[cause] web3::Error),
-}
-
-impl From<std::fmt::Error> for Error {
-    fn from(err: std::fmt::Error) -> Self {
-        Error::Fmt(err)
-    }
-}
-
-impl From<std::io::Error> for Error {
-    fn from(err: std::io::Error) -> Self {
-        Error::Io(err)
-    }
-}
-
-impl From<serde_json::Error> for Error {
-    fn from(err: serde_json::Error) -> Self {
-        Error::Json(err)
-    }
-}
-
-impl From<web3::Error> for Error {
-    fn from(err: web3::Error) -> Self {
-        Error::Rpc(err)
-    }
-}
