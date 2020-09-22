7cc6c774beeb627f1fd83c1abebdd8a6d201d683
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2020-09-23 00:03:13 +0800

    Add Session support (#18)
    * Add Session support
    
    * Revert xpallets and signer prompt

---
+++ src/app/session.rs
@@ -0,0 +1,35 @@
+use anyhow::Result;
+use substrate_subxt::session::ValidatorsStoreExt;
+
+use crate::utils::{build_client, Sr25519Signer};
+
+#[derive(structopt::StructOpt, Debug)]
+pub enum Session {
+    #[structopt(name = "set-keys")]
+    SetKeys {
+        #[structopt(index = 1, long)]
+        keys: String,
+    },
+    #[structopt(name = "validators")]
+    Validators,
+}
+
+impl Session {
+    pub async fn run(self, url: String, _signer: Sr25519Signer) -> Result<()> {
+        let client = build_client(url).await?;
+
+        match self {
+            Self::Validators => {
+                println!("{:#?}", client.validators(None).await?);
+            }
+            Self::SetKeys { keys } => {
+                let _ = keys;
+                todo!()
+                // let result = client.set_keys_and_watch(&signer, &call).await?;
+                // println!("{:#?}", result);
+            }
+        }
+
+        Ok(())
+    }
+}
