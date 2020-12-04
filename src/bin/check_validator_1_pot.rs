8c0c49325b1f2dc852e44272d0044895931b4cdf
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2020-12-04 09:51:11 +0800

    .

---
+++ src/bin/check_validator_1_pot.rs
@@ -0,0 +1,43 @@
+use anyhow::Result;
+use subxt::{
+    balances::AccountData,
+    system::{AccountInfo, AccountStoreExt},
+};
+
+use chainx_cli::{block_hash, build_client, parse_account, rpc::Rpc};
+use frame_support::sp_std::collections::btree_map::BTreeMap;
+
+fn pretty_display(balance: u128) -> String {
+    format!("{}.{}", balance / 100_000_000, balance % 100_000_000)
+}
+
+#[async_std::main]
+async fn main() -> Result<()> {
+    env_logger::init();
+
+    // let url = "ws://116.62.46.8:8087";
+    let url = "ws://127.0.0.1:8087";
+    let block_number: u32 = 120_000;
+
+    let client = build_client(url.clone()).await?;
+    let who = parse_account("5SpVGHLk3RtmehCq1TbZ8HrvGnEpq4ZFSQUAorb6g6F2RRZi").unwrap();
+
+    let mut last_balance = 0;
+    let mut new_diff = 0;
+    for i in 0..block_number {
+        let at = block_hash(&client, Some(i)).await?;
+        let account_info = client.account(&who, at).await?;
+        let new_free = account_info.data.free;
+        if new_free != last_balance {
+            let diff = new_free - last_balance;
+            if diff != new_diff {
+                println!("========= New diff: {}", diff);
+                new_diff = diff;
+            }
+            println!("New free {} at Block {}, diff: {}", new_free, i, diff);
+            last_balance = new_free;
+        }
+    }
+
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -6,4 +6,6 @@ mod utils;
 
 pub use self::app::App;
 pub use self::serde::{serde_hex, serde_num_str, serde_text};
-pub use self::utils::{block_hash, build_client, get_account_id_from_seed, get_from_seed};
+pub use self::utils::{
+    block_hash, build_client, get_account_id_from_seed, get_from_seed, parse_account,
+};
