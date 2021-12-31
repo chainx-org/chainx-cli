ff67747ec8e3f41e0a3a48c0260e06af1f549c6d
icodezjb <8869892+icodezjb@users.noreply.github.com>
2021-12-31 10:45:03 +0800

    KSX airdrop tools and data (#37)
    * Reexport snapshot data
    
    * Add more info in file path
    
    * Fix vesting path
    
    * Add check_snapshot which are companion with snapshot_balances
    
    * Rm workflow
    
    * Add resource files
    
    * Remove vesting balances
    
    * Set MINIMUM_AIRDROP_BALANCE = 100 PCX
    
    * Regenerate snapshot zero and non-zero balances
    
    * Save some works
    
    * Print more info
    
    * Regenerate genesis files
    
    Co-authored-by: icodezjb <icodezjb@users.noreply.github.com>

---
+++ src/bin/generate_airdrop.rs
@@ -0,0 +1,415 @@
+use chainx_cli::{
+    runtime::primitives::{AccountId, BlockNumber},
+};
+use anyhow::Result;
+
+use sp_core::crypto::{set_default_ss58_version, Ss58AddressFormat};
+use sp_runtime::{
+    traits::AccountIdConversion, ModuleId,
+};
+
+#[derive(serde::Serialize, serde::Deserialize)]
+pub struct SherpaXBalances{
+    pub balances: Vec<(AccountId, u128)>
+}
+
+#[derive(serde::Serialize, serde::Deserialize)]
+pub struct SherpaXVesting {
+    // * who - Account which we are generating vesting configuration for
+    // * begin - Block when the account will start to vest
+    // * length - Number of blocks from `begin` until fully vested
+    // * liquid - Number of units which can be spent before vesting begins
+    pub vesting: Vec<(AccountId, BlockNumber, BlockNumber, u128)>
+}
+
+/// Struct to encode the vesting schedule of an individual account.
+#[derive(serde::Serialize, serde::Deserialize)]
+pub struct SherpaXSchedule {
+    // * who - Account which we are generating vesting configuration for
+    // * locked - Locked amount at genesis.
+    // * per_block - Amount that gets unlocked every block after `starting_block`.
+    // * starting_block - Starting block for unlocking(vesting).
+    pub schedules: Vec<(AccountId, u128, u128, BlockNumber)>
+}
+
+macro_rules! balances {
+    ($file:expr, $total_accounts:expr, $total_balance:expr) => {{
+        let file = std::fs::File::open($file)
+            .map_err(|e| format!("Error opening balances json file: {}", e))?;
+
+        let mut config: SherpaXBalances = serde_json::from_reader(file)
+            .map_err(|e| format!("Error parsing balances json file: {}", e))?;
+
+        config.balances.dedup_by_key(|(account, _)| account.clone());
+
+        let total = config.balances.iter().map(|(_, b)| b).sum::<u128>();
+
+        assert_eq!($total_accounts, config.balances.len());
+        assert_eq!($total_balance, total);
+
+        config
+    }};
+}
+
+pub mod configs {
+    use super::*;
+
+    // use for check_duplicate
+    pub fn origin_balances() -> Result<Vec<SherpaXBalances>, String> {
+        Ok(
+            vec![
+                balances!(
+                    concat!(
+                        env!("CARGO_MANIFEST_DIR"),
+                        "/origin_chainx_snapshot1_non_dust_7418_10500000000000000000000000_on_2761158.json"
+                    ),
+                    7418,
+                    10500000000000000000000000
+                ),
+                balances!(
+                    concat!(
+                        env!("CARGO_MANIFEST_DIR"),
+                        "/origin_comingchat_miners_334721_214074281900000_decimal_8.json"
+                    ),
+                    334721,
+                    214074281900000
+                ),
+                balances!(
+                    concat!(
+                        env!("CARGO_MANIFEST_DIR"),
+                        "/origin_sherpax_contributors_1873_9404698487265_decimal_8.json"
+                    ),
+                    1873,
+                    9404698487265
+                )
+            ]
+        )
+    }
+
+    // split (snapshot1_balances - snapshot2_balances) to transfer vesting
+    pub fn filter_snapshots() -> Result<SherpaXBalances, String> {
+        let mut ss1 = balances!(
+                    concat!(
+                        env!("CARGO_MANIFEST_DIR"),
+                        "/origin_chainx_snapshot1_non_dust_7418_10500000000000000000000000_on_2761158.json"
+                    ),
+                    7418,
+                    10500000000000000000000000
+                );
+        let ss2 = balances!(
+                    concat!(
+                        env!("CARGO_MANIFEST_DIR"),
+                        "/origin_chainx_snapshot2_non_dust_22295_11985224700000000000000000_on_2004141.json"
+                    ),
+                    22295,
+                    11985224700000000000000000
+                );
+
+        // Skip 5S7WgdAXVK7mh8REvXfk9LdHs3Xqu9B2E9zzY8e4LE8Gg2ZX
+        let treasury_account: AccountId = ModuleId(*b"pcx/trsy").into_account();
+
+        let mut to_genesis = SherpaXBalances {
+            balances: vec![]
+        };
+        let mut to_transfer = SherpaXBalances {
+            balances: vec![]
+        };
+
+        let treasury = ss1
+            .balances
+            .iter()
+            .find(|t| t.0 == treasury_account)
+            .unwrap()
+            .clone();
+
+        println!("treasury balance: {:?}", treasury.1);
+
+        ss1.balances.retain(|(account,_)| *account != treasury_account);
+        const MIN_BALANCE: u128 = 1_000_000_000_000_000_000;
+
+        for e1 in ss1.balances {
+            match ss2.balances.iter().find(|e2| e2.0 == e1.0) {
+                Some(e2) if e1.1 > e2.1 && e1.1.saturating_sub(e2.1) >= MIN_BALANCE => {
+                    let transfer = e1.1.saturating_sub(e2.1);
+
+                    if e2.1 != 0 {
+                        to_genesis.balances.push(e2.clone())
+                    }
+
+                    to_transfer.balances.push((e1.0, transfer))
+                },
+                _ => {
+                    to_genesis.balances.push(e1)
+                }
+            }
+        }
+
+        // add treasury
+        to_genesis.balances.push(treasury);
+
+        let total = to_genesis.balances.iter().map(|(_, b)| b).sum::<u128>();
+        let prefix = format!(
+            "genesis_balances_chainx_snapshot_{}_{}",
+            to_genesis.balances.len(),
+            total
+        );
+
+        to_genesis.balances.sort_unstable_by_key(|(_, b)| *b);
+
+        to_file::<SherpaXBalances>(&prefix, &to_genesis)
+            .map_err(|e| format!("{:?}", e))?;
+
+        let total = to_transfer.balances.iter().map(|(_, b)| b).sum::<u128>();
+        let prefix = format!(
+            "transfer_balances_{}_{}",
+            to_transfer.balances.len(),
+            total
+        );
+
+        to_transfer.balances.sort_unstable_by_key(|(_, b)| *b);
+
+        to_file::<SherpaXBalances>(&prefix, &to_transfer)
+            .map_err(|e| format!("{:?}", e))?;
+
+        to_vesting_transfer(to_transfer)?;
+
+        Ok(to_genesis)
+    }
+
+    pub fn genesis_balances() -> Result<Vec<SherpaXBalances>, String> {
+        Ok(
+            vec![
+                balances!(
+                    concat!(
+                        env!("CARGO_MANIFEST_DIR"),
+                        "/genesis_balances_chainx_snapshot_7418_7868415220855310000000000.json"
+                    ),
+                    7418,
+                    7868415220855310000000000u128
+                ),
+                balances!(
+                    concat!(
+                        env!("CARGO_MANIFEST_DIR"),
+                        "/genesis_balances_comingchat_miners_334721_2140742819000000000000000.json"
+                    ),
+                    334721,
+                    2140742819000000000000000u128
+                ),
+                balances!(
+                    concat!(
+                        env!("CARGO_MANIFEST_DIR"),
+                        "/genesis_balances_sherpax_contributors_1873_94046984872650000000000.json"
+                    ),
+                    1873,
+                    94046984872650000000000u128
+                )
+            ]
+        )
+    }
+
+    pub fn check_genesis_balances() {
+        let mut balances: Vec<(AccountId, u128)> = genesis_balances()
+            .unwrap()
+            .into_iter()
+            .flat_map(|s| s.balances)
+            .collect();
+
+        balances.dedup_by_key(|(account, _)| account.clone());
+
+        assert_eq!(
+            balances.len(),
+            7418 + 334721 + 1873,
+            "Need manual process duplicate account balance"
+        );
+
+        let total = balances.iter().map(|(_, b)| b).sum::<u128>();
+        assert_eq!(
+            total,
+            7868415220855310000000000u128
+                .saturating_add(2140742819000000000000000u128)
+                .saturating_add(94046984872650000000000u128),
+        );
+
+        println!("total genesis balances: {:?}", total);
+        println!("total genesis accounts: {:?}", balances.len());
+    }
+
+    pub fn balances_chainx_snapshot() -> Result<Vec<SherpaXBalances>, String> {
+        Ok(vec![filter_snapshots()?])
+    }
+
+    pub fn balances_comingchat_mine() -> Result<Vec<SherpaXBalances>, String> {
+        let mut origin = balances!(
+            concat!(env!("CARGO_MANIFEST_DIR"), "/origin_comingchat_miners_334721_214074281900000_decimal_8.json"),
+            334721,
+            214074281900000u128
+        );
+
+        translate_decimal_from_8_to_18_balances(&mut origin);
+
+        origin.balances.sort_unstable_by_key(|(_, b)| *b);
+
+        to_file::<SherpaXBalances>(
+            "genesis_balances_comingchat_miners_334721_2140742819000000000000000",
+            &origin
+        )
+            .map_err(|e| format!("{:?}", e))?;
+
+        Ok(vec![origin])
+    }
+
+    pub fn balances_sherpax_crowdloan() -> Result<Vec<SherpaXBalances>, String> {
+        let mut origin = balances!(
+            concat!(env!("CARGO_MANIFEST_DIR"), "/origin_sherpax_contributors_1873_9404698487265_decimal_8.json"),
+            1873,
+            9404698487265u128
+        );
+
+        translate_decimal_from_8_to_18_balances(&mut origin);
+
+        origin.balances.sort_unstable_by_key(|(_, b)| *b);
+
+        to_file::<SherpaXBalances>("genesis_balances_sherpax_contributors_1873_94046984872650000000000", &origin)
+            .map_err(|e| format!("{:?}", e))?;
+
+        Ok(vec![origin])
+    }
+
+    pub fn check_origin_duplicate() {
+        let mut balances: Vec<(AccountId, u128)> = origin_balances()
+            .unwrap()
+            .into_iter()
+            .flat_map(|s| s.balances)
+            .collect();
+
+        balances.dedup_by_key(|(account, _)| account.clone());
+
+        assert_eq!(
+            7418 + 334721 + 1873,
+            balances.len(),
+            "Need manual process duplicate account balance"
+        )
+    }
+
+    pub fn to_vesting_genesis() -> Result<SherpaXVesting, String> {
+        // Skip 5S7WgdAXVK7mh8REvXfk9LdHs3Xqu9B2E9zzY8e4LE8Gg2ZX
+        let treasury_account: AccountId = ModuleId(*b"pcx/trsy").into_account();
+
+        let vesting: Vec<(AccountId, BlockNumber, BlockNumber, u128)> = genesis_balances()?
+            .into_iter()
+            .flat_map(|s| s.balances)
+            .filter_map(|(account, free)|{
+                if account == treasury_account {
+                    return None
+                }
+                Some((account, 1296000, 2592000, free.saturating_div(10u128)))
+            })
+            .collect();
+
+        let vesting_free = vesting
+            .iter()
+            .map(|(_,_,_,free)|free)
+            .sum::<u128>();
+        let vesting_accounts = vesting.len();
+
+        assert_eq!(
+            vesting_accounts,
+            7418 + 334721 + 1873 - 1
+        );
+        assert_eq!(
+            vesting_free.saturating_mul(10),
+            7868415220855310000000000u128
+                .saturating_add(2140742819000000000000000u128)
+                .saturating_add(94046984872650000000000u128)
+                .saturating_sub(1067642049647850000000000)
+        );
+
+        let v = SherpaXVesting{ vesting };
+
+        let prefix = format!("genesis_vesting_{}_{}", vesting_accounts, vesting_free);
+        to_file::<SherpaXVesting>(&prefix, &v)
+            .map_err(|e| format!("{:?}", e))?;
+
+        Ok(v)
+    }
+
+    pub fn to_vesting_transfer(to_transfer: SherpaXBalances) -> Result<(), String> {
+        let accounts = to_transfer.balances.len();
+        let total = to_transfer.balances.iter().map(|(_, b)| b).sum::<u128>();
+
+        let schedules: Vec<(AccountId, u128, u128, BlockNumber)> = to_transfer
+            .balances
+            .into_iter()
+            .map(|(account, balance)|{
+                (account, balance, balance.saturating_div(5184000), 3888000)
+            })
+            .collect();
+
+        let total_locks = schedules.iter().map(|(_, b, _, _)| b).sum::<u128>();
+
+        assert_eq!(schedules.len(), accounts);
+        assert_eq!(total_locks, total);
+
+        let prefix = format!("transfer_vesting_{}_{}", accounts, total);
+        to_file::<SherpaXSchedule>(&prefix, &SherpaXSchedule{ schedules })
+            .map_err(|e| format!("{:?}", e))
+    }
+
+    pub fn to_file<V>(prefix: &str, value: &V) -> Result<()>
+        where V: ?Sized + serde::Serialize,
+    {
+        let mut output = std::env::current_dir()?;
+        output.push(format!("{}.json", prefix));
+
+        let file = std::fs::OpenOptions::new()
+            .create(true)
+            .write(true)
+            .open(output)?;
+
+        Ok(serde_json::to_writer_pretty(file, value)?)
+    }
+
+    pub fn translate_decimal_from_8_to_18_balances(origin: &mut SherpaXBalances) {
+        let new_balances: Vec<(AccountId, u128)>  = origin
+            .balances
+            .iter()
+            .map(|(account, free)| (account.clone(), free.saturating_mul(10_000_000_000)))
+            .collect();
+
+        origin.balances = new_balances
+    }
+
+    pub fn translate_decimal_from_12_to_18_balances(origin: &mut SherpaXBalances) {
+        let new_balances: Vec<(AccountId, u128)>  = origin
+            .balances
+            .iter()
+            .map(|(account, free)| (account.clone(), free.saturating_mul(1_000_000)))
+            .collect();
+
+        origin.balances = new_balances
+    }
+}
+
+#[async_std::main]
+async fn main() -> Result<(), String> {
+    set_default_ss58_version(Ss58AddressFormat::ChainXAccount);
+
+    // 1. check origin balances
+    crate::configs::check_origin_duplicate();
+
+    // 2. filter origin balances to
+    // 2.1) save genesis balances
+    // 2.2) save transfer balances
+    // 2.3) save transfer vesting
+    crate::configs::balances_sherpax_crowdloan()?;
+    crate::configs::balances_comingchat_mine()?;
+    crate::configs::balances_chainx_snapshot()?;
+
+    // 3. check genesis balances
+    crate::configs::check_genesis_balances();
+
+    // 4. generate genesis vesting
+    crate::configs::to_vesting_genesis()?;
+
+    Ok(())
+}
--- src/bin/snapshot_balances.rs
+++ src/bin/snapshot_balances.rs
@@ -38,8 +38,12 @@ struct App {
     /// Ss58 Address version of the network.
     ///
     /// 44 for ChainX mainnet, 42 for Substrate.
+    #[allow(dead_code)]
     #[structopt(long, default_value = "44")]
     pub ss58_prefix: sp_core::crypto::Ss58AddressFormat,
+
+    #[structopt(long, default_value = "100_000_000")]
+    pub min_balance: Balance
 }
 
 pub type ChainBlock<T> = SignedBlock<Block<<T as System>::Header, <T as System>::Extrinsic>>;
@@ -59,7 +63,8 @@ where
     V: ?Sized + serde::Serialize,
 {
     let mut output = std::env::current_dir()?;
-    output.push(format!("{}_snapshot_{}.json", prefix, block_number));
+    output.push(format!("{}_on_{}.json", prefix, block_number));
+
     let file = std::fs::OpenOptions::new()
         .create(true)
         .write(true)
@@ -79,6 +84,18 @@ struct BalanceRecord {
 struct KsxAccount {
     account_id: AccountId,
     free: Balance,
+}
+
+#[derive(serde::Serialize, serde::Deserialize)]
+struct SherpaXBalances{
+    pub balances: Vec<(AccountId, u128)>
+}
+impl SherpaXBalances {
+    fn from(b: Vec<(AccountId, u128)>) -> Self {
+        SherpaXBalances{
+            balances: b
+        }
+    }
 }
 
 #[async_std::main]
@@ -103,15 +120,18 @@ async fn main() -> Result<()> {
 
     let account_info = rpc.get_accounts_info(at).await?;
 
-    /// Minimum balance to receive KSX airdrop.
-    const MINIMUM_AIRDROP_BALANCE: Balance = 100_000_000;
+    // Minimum balance to receive KSX airdrop.
+    let min_balance: Balance = app.min_balance;
 
     let mut total_issuance = 0u128;
 
     let mut dust_count = 0;
     let mut dust_sum = 0;
+    // 5ScUq4UWtp4Tpve8e6YJoWhXDFpapVwiqxjk3drMtEvpR2y9
+    let x_association: AccountId = hex_literal::hex!("8387441be6459881fb86af8e36254d537a9d2b86374a553176380811163b7441").into();
+    let mut x_association_balance = 0;
 
-    let (mut ksx_accounts, dust_accounts): (
+    let (mut non_dust_accounts, dust_accounts): (
         Vec<Option<BalanceRecord>>,
         Vec<Option<BalanceRecord>>,
     ) = account_info
@@ -121,6 +141,11 @@ async fn main() -> Result<()> {
 
             total_issuance += total;
 
+            if id == x_association {
+                x_association_balance = total;
+                return (None, None)
+            }
+
             let maybe_ignored = BalanceRecord {
                 account_id: id,
                 free: info.data.free,
@@ -128,7 +153,7 @@ async fn main() -> Result<()> {
                 total,
             };
 
-            if total < MINIMUM_AIRDROP_BALANCE {
+            if total < min_balance {
                 dust_count += 1;
                 dust_sum += total;
                 (None, Some(maybe_ignored))
@@ -139,15 +164,24 @@ async fn main() -> Result<()> {
         .unzip();
 
     let treasury_account: AccountId = ModuleId(*b"pcx/trsy").into_account();
+    let mut treasury_balance = 0u128;
+    let mut new_treasury_balance = 0u128;
 
-    let (ksx_accounts, dust_accounts): (Vec<_>, Vec<_>) = (
-        ksx_accounts
+    let (non_dust_accounts, dust_accounts): (Vec<_>, Vec<_>) = (
+        non_dust_accounts
             .iter_mut()
             .flatten()
             .map(|record| {
                 if record.account_id == treasury_account {
+                    treasury_balance = record.total;
+
+                    record.free += x_association_balance;
+                    record.total += x_association_balance;
+
                     record.free += dust_sum;
                     record.total += dust_sum;
+
+                    new_treasury_balance = record.total;
                 }
                 KsxAccount {
                     free: record.total,
@@ -158,20 +192,72 @@ async fn main() -> Result<()> {
         dust_accounts.into_iter().flatten().collect(),
     );
 
-    save_snapshot(block_number, "ksx_accounts", &ksx_accounts)?;
-    save_snapshot(block_number, "dust_accounts", &dust_accounts)?;
+    let non_dust_balances: Vec<(AccountId, u128)> = non_dust_accounts
+        .iter()
+        .map(|non_dust_account|{
+            (non_dust_account.account_id.clone(), non_dust_account.free.saturating_mul(10_000_000_000))
+        })
+        .collect();
+    let dust_balances: Vec<(AccountId, u128)> = dust_accounts
+        .iter()
+        .map(|dust_account|{
+            (dust_account.account_id.clone(), dust_account.total.saturating_mul(10_000_000_000))
+        })
+        .collect();
 
-    println!("       Total issuance: {}", total_issuance);
-    let total_accounts = ksx_accounts.len() + dust_accounts.len();
-    println!("       Total accounts: {}", total_accounts);
-    println!("         KSX accounts: {}", ksx_accounts.len());
-    println!("Dust accounts(< 1PCX): {}", dust_count);
-    println!("  Total dust balances: {}", dust_sum);
+    println!("   On ChainX(decimals=8)  ");
+    println!("        Total issuance: {}", total_issuance);
+    let total_accounts = non_dust_accounts.len() + dust_accounts.len();
+    println!("        Total accounts: {}", total_accounts);
+    println!("     Non-dust accounts: {}", non_dust_accounts.len());
+    println!("Minim balance for dust: {}", min_balance);
+    println!("         Dust accounts: {}", dust_count);
+    println!("   Total dust balances: {}", dust_sum);
+    println!("      Treasury balance: {}", treasury_balance);
+    println!(" X-association balance: {}", x_association_balance);
 
     // Verify
-    let total_ksx = ksx_accounts.iter().map(|r| r.free).sum::<Balance>();
+    let total_pcx = non_dust_accounts
+        .iter()
+        .map(|r| r.free)
+        .sum::<Balance>();
+    assert_eq!(total_pcx, total_issuance);
 
-    assert_eq!(total_ksx, total_issuance);
+    let total_ksx = non_dust_balances
+        .iter()
+        .map(|(_, balance)|balance)
+        .sum::<u128>();
+
+    assert_eq!(total_pcx.saturating_mul(10_000_000_000), total_ksx);
+    assert_eq!(total_accounts, non_dust_balances.len() + dust_balances.len());
+
+    assert_eq!(dust_count, dust_accounts.len());
+    assert_eq!(total_accounts, non_dust_accounts.len() + dust_accounts.len());
+
+    let total_dust = dust_balances.iter().map(|(_, b)|b).sum::<u128>();
+    assert_eq!(dust_sum.saturating_mul(10_000_000_000), total_dust);
+    let total_non_dust = non_dust_balances.iter().map(|(_, b)|b).sum::<u128>();
+    assert_eq!(total_ksx, total_non_dust);
+
+    let new_treasury_balance = new_treasury_balance.saturating_mul(10_000_000_000);
+
+    println!("==========================");
+    println!("  On SherpaX(decimals=18) ");
+    println!("     Total airdrop ksx: {}", total_ksx);
+    println!("        Total accounts: {}", total_accounts);
+    println!("     Non-dust accounts: {}", non_dust_balances.len());
+    println!("Minim balance for dust: {}", min_balance.saturating_mul(10_000_000_000));
+    println!("         Dust accounts: 0");
+    println!("   Total dust balances: 0");
+    println!("      Treasury balance: {}", new_treasury_balance);
+    println!(" X-association balance: 0");
+    println!("Total non-dust balance: {}", total_non_dust);
+
+    let non_dust_prefix = format!("origin_chainx_snapshot_non_dust_{}_{}", non_dust_accounts.len(), total_non_dust);
+    let dust_prefix = format!("origin_chainx_snapshot_dust_{}_{}", dust_accounts.len(), total_dust);
+
+    save_snapshot(block_number, non_dust_prefix , &SherpaXBalances::from(non_dust_balances))?;
+    save_snapshot(block_number, dust_prefix, &SherpaXBalances::from(dust_balances))?;
 
     Ok(())
 }
